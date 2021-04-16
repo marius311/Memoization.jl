2c04354b2dee10e1c70d8774452be0a21dcdbd7f
marius <mariusmillea@gmail.com>
2021-04-16 13:08:42 -0700

    WIP

--- src/Memoization.jl
+++ src/Memoization.jl
@@ -4,7 +4,7 @@ using MacroTools: splitdef, combinedef, splitarg, combinearg, isexpr
 export @memoize
 
 # Stores a mapping (func => cache) for each memoized function `func`.
-const caches = WeakKeyDict()
+const caches = IdDict()
 
 # Stores a mapping (cache_constructor_exprs => cache) for each
 # memoized function `func`, so that we can verify the same expression 
@@ -13,15 +13,16 @@ const cache_constructor_exprs = IdDict()
 # Non-function "callables" or any function that has closed-over variables
 # attached to it (indicated by having type parameters) are memoized
 # per-instance. 
-memoize_per_instance(::Type{F}) where {F<:Function} = !isempty(F.parameters)
-memoize_per_instance(::Type) = true
+statically_memoizable(::Type{F}) where {F<:Function} = isempty(F.parameters)
+statically_memoizable(::Type) = false
 
-# Look up (and possibly create) the cache for a given memoized function. Using a
-# generated function allows us to move this lookup to compile time for top-level
-# functions and improve performance.
-@generated get_cache(func::F) where {F} =
-    memoize_per_instance(F) ? :($caches[func]) : caches[F.instance]
-    
+
+# Look up the cache for a given memoized function. Using a generated
+# function allows us to move this lookup to compile time for
+# statically memoizable functions and improve performance.
+@generated function get_cache(default::Base.Callable, func::F) where {F}
+    statically_memoizable(F) ? caches[F.instance] : :(_get!(default, $caches, func))
+end
 
 """
     empty_cache!(arg)
@@ -34,16 +35,16 @@ matching that type will be cleared.
 """
 empty_cache!(func) = map(empty!, values(find_caches(func)))
 find_caches(func::F) where {F<:Function} = 
-    filter((((func′,_),_),)->(memoize_per_instance(F) ? (func′ == func) : (func′ == F)), caches)
+    filter(((func′,_),)->(statically_memoizable(F) ? (func′ == func) : (func′ == F)), caches)
 find_caches(F::Union{DataType,Union,UnionAll}) = 
-    filter((((func′,_),_),)->(func′ isa F), caches)
+    filter(((func′,_),)->(func′ isa F), caches)
 empty_all_caches!() = map(empty!, values(caches))
 
 """
     @memoize f(x) = ...
     @memoize Dict f(x) = ... # with custom cache type
     @memoize LRU(maxsize=4) f(x) = ... # with custom cache type
-    @memoize (::Foo)(x) = ... # memoizing a callable
+    @memoize (x::Foo)(y) = ... # memoizing a callable (on both x and y)
     f(x) = @memoize g(y) = ... # memoizing a closure
 
 Memoize a function, closure, or callable, with respect to all of its arguments
@@ -61,11 +62,11 @@ based on the contents of vectors, you could use a `Dict`.
 macro memoize(ex1, ex2=nothing)
     cache_constructor, funcdef = ex2 == nothing ? (IdDict, ex1) : (ex1, ex2)
     sdef = splitdef(funcdef)
-    # if cache_constructor is a call, wrap it in a () -> ...
-    if isexpr(cache_constructor,:call)
-        cache_constructor = (:(() -> $cache_constructor),)
-    end
     cache_constructor_expr = QuoteNode(Base.remove_linenums!(cache_constructor))
+    # if cache_constructor is a call, wrap it in a () -> ...
+    if isexpr(cache_constructor, :call)
+        cache_constructor = :(() -> $cache_constructor)
+    end
     # give unnamed args a placeholder name:
     sdef[:args] = map(sdef[:args]) do arg
         sarg = splitarg(arg)
@@ -73,7 +74,7 @@ macro memoize(ex1, ex2=nothing)
     end
     arg_signature   = [(issplat ? :($arg...) : arg)          for (arg,_,issplat) in map(splitarg,sdef[:args])]
     kwarg_signature = [(issplat ? :($arg...) : :($arg=$arg)) for (arg,_,issplat) in map(splitarg,sdef[:kwargs])]
-    T, getter = (gensym.(("T","getter")))
+    T, getter = gensym.(("T","getter"))
     
     # if memoizing just `f(x) = ...` we want to call both `get_cache` and
     # `empty_cache` on `f`, but if memoizing a callable like
@@ -89,35 +90,31 @@ macro memoize(ex1, ex2=nothing)
     
     # the body of the function definition is replaced with this:
     sdef[:body] = quote
-        ($getter)() = $(sdef[:body])
-        $T = $(Core.Compiler.return_type)($getter, $Tuple{})
-        $_get!($getter, $get_cache($cacheid_get), (($(arg_signature...),),(;$(kwarg_signature...),))) :: $T
+        # ($getter)() = $(sdef[:body])
+        # $T = $(Core.Compiler.return_type)($getter, $Tuple{})
+        # $_get!($getter, $get_cache($cache_constructor, $cacheid_get), (($(arg_signature...),),(;$(kwarg_signature...),))) :: $T
+        $cacheid_get
     end
     
 
     canary = gensym("canary")
     quote
         func = Core.@__doc__ $(esc(combinedef(sdef)))
-        # only empty cache if this function definition is at the toplevel
-        # see also: https://discourse.julialang.org/t/is-there-a-way-to-determine-whether-code-is-toplevel
-        $(esc(canary)) = true
-        if isdefined($__module__, $(QuoteNode(canary)))
-            # $empty_cache!($(esc(cacheid_empty)))
-        end
-        # verify we haven't already memoized this function with some other cache type
-        begin
-            local cache = _get!($(esc(cache_constructor)), caches, func)
-            if $(esc(cache_constructor)) isa Type
-                if !(cache isa $(esc(cache_constructor)))
-                    error("All methods must use same cache type, but $func is already memoized with $(typeof(cache))")
-                end
-            else
-                local cache_constructor_expr′ = _get!(()->$cache_constructor_expr, cache_constructor_exprs, func)
-                if cache_constructor_expr′ != $cache_constructor_expr
-                    error("All methods must use same cache type, but $func is already memoized with:\n $cache_constructor_expr′")
-                end
-            end
-        end
+        # begin
+        #     # for statically memoizable functions, create the cache here if it doesnt exist
+        #     $statically_memoizable(typeof(func)) && _get!($(esc(cache_constructor)), caches, func)
+        #     # verify we haven't switched the cache type
+        #     local cache_constructor_expr′ = _get!(()->$cache_constructor_expr, cache_constructor_exprs, func)
+        #     if cache_constructor_expr′ != $cache_constructor_expr
+        #         error("$func is already memoized with $cache_constructor_expr′")
+        #     end
+        # end
+        # # empty cache, but only if this is a top-level function definition
+        # # see also: https://discourse.julialang.org/t/is-there-a-way-to-determine-whether-code-is-toplevel
+        # $(esc(canary)) = true
+        # if isdefined($__module__, $(QuoteNode(canary)))
+        #     $empty_cache!($(esc(cacheid_empty)))
+        # end
         func
     end
 end
