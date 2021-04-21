marius311/Memoization.jl

commit 2cb70535648c82e735692aa17adbf25a157b4a66

Rework memoization internals and remove world-age issues (#12)

src/Memoization.jl
@@ -3,33 +3,25 @@ module Memoization
 using MacroTools: splitdef, combinedef, splitarg, combinearg, isexpr
 export @memoize
 
-# Stores a mapping:
-# 
-#    (func, cache_type) => cache
-# 
-# for each memoized function `func`. For top-level functions, `func` is the
-# function's type. For closures or callables, `func` is an individual instance
-# of the closure or callable object.
+# Stores a mapping (func => cache) for each memoized function `func`.
 const caches = IdDict()
+
+# Stores a mapping (cache_constructor_exprs => cache) for each
+# memoized function `func`, so that we can verify the same expression 
+const cache_constructor_exprs = IdDict()
 
 # Non-function "callables" or any function that has closed-over variables
 # attached to it (indicated by having type parameters) are memoized
 # per-instance. 
-memoize_per_instance(::Type{F}) where {F<:Function} = !isempty(F.parameters)
-memoize_per_instance(::Type) = true
+statically_memoizable(::Type{F}) where {F<:Function} = isempty(F.parameters)
+statically_memoizable(::Type) = false
 
-# Look up (and possibly create) the cache for a given memoized function. Using a
-# generated function allows us to move this lookup to compile time for top-level
-# functions and improve performance.
-@generated function get_cache(func::F, cache_type=IdDict) where {F}
-    if memoize_per_instance(F) || !(cache_type <: Type)
-        quote
-            _get!(cache_type, $caches, (func,cache_type))
-        end
-    else
-        CT, = cache_type.parameters
-        _get!(CT, caches, (F,CT))
-    end
+
+# Look up the cache for a given memoized function. Using a generated
+# function allows us to move this lookup to compile time for
+# statically memoizable functions and improve performance.
+@generated function get_cache(default::Base.Callable, func::F) where {F}
+    statically_memoizable(F) ? caches[F.instance] : :(_get!(default, $caches, func))
 end
 
 """
@@ -43,16 +35,16 @@ matching that type will be cleared.
 """
 empty_cache!(func) = map(empty!, values(find_caches(func)))
 find_caches(func::F) where {F<:Function} = 
-    filter((((func′,_),_),)->(memoize_per_instance(F) ? (func′ == func) : (func′ == F)), caches)
+    filter(((func′,_),)->(statically_memoizable(F) ? (func′ == func) : (func′ == F)), caches)
 find_caches(F::Union{DataType,Union,UnionAll}) = 
-    filter((((func′,_),_),)->(func′ isa F), caches)
+    filter(((func′,_),)->(func′ isa F), caches)
 empty_all_caches!() = map(empty!, values(caches))
 
 """
     @memoize f(x) = ...
     @memoize Dict f(x) = ... # with custom cache type
     @memoize LRU(maxsize=4) f(x) = ... # with custom cache type
-    @memoize (::Foo)(x) = ... # memoizing a callable
+    @memoize (x::Foo)(y) = ... # memoizing a callable (on both x and y)
     f(x) = @memoize g(y) = ... # memoizing a closure
 
 Memoize a function, closure, or callable, with respect to all of its arguments
@@ -68,11 +60,12 @@ the macro before the function definition. For example, if you want to memoize
 based on the contents of vectors, you could use a `Dict`.
 """
 macro memoize(ex1, ex2=nothing)
-    cachetype, funcdef = ex2 == nothing ? ((), ex1) : ((ex1,), ex2)
+    cache_constructor, funcdef = ex2 == nothing ? (IdDict, ex1) : (ex1, ex2)
     sdef = splitdef(funcdef)
-    # if cachetype is a call, wrap it in a () -> ...
-    if !isempty(cachetype) && isexpr(cachetype[1],:call)
-        cachetype = (:(() -> $(cachetype[1])),)
+    cache_constructor_expr = QuoteNode(Base.remove_linenums!(cache_constructor))
+    # if cache_constructor is a call, wrap it in a () -> ...
+    if isexpr(cache_constructor, :call)
+        cache_constructor = :(() -> $cache_constructor)
     end
     # give unnamed args a placeholder name:
     sdef[:args] = map(sdef[:args]) do arg
@@ -81,7 +74,7 @@ macro memoize(ex1, ex2=nothing)
     end
     arg_signature   = [(issplat ? :($arg...) : arg)          for (arg,_,issplat) in map(splitarg,sdef[:args])]
     kwarg_signature = [(issplat ? :($arg...) : :($arg=$arg)) for (arg,_,issplat) in map(splitarg,sdef[:kwargs])]
-    T, getter = (gensym.(("T","getter")))
+    T, getter = gensym.(("T","getter"))
     
     # if memoizing just `f(x) = ...` we want to call both `get_cache` and
     # `empty_cache` on `f`, but if memoizing a callable like
@@ -99,13 +92,23 @@ macro memoize(ex1, ex2=nothing)
     sdef[:body] = quote
         ($getter)() = $(sdef[:body])
         $T = $(Core.Compiler.return_type)($getter, $Tuple{})
-        $_get!($getter, $get_cache($cacheid_get, $(cachetype...)), (($(arg_signature...),),(;$(kwarg_signature...),))) :: $T
+        $_get!($getter, $get_cache($cache_constructor, $cacheid_get), (($(arg_signature...),),(;$(kwarg_signature...),))) :: $T
     end
     
+
     canary = gensym("canary")
     quote
         func = Core.@__doc__ $(esc(combinedef(sdef)))
-        # only empty cache if this function definition is at the toplevel
+        begin
+            # for statically memoizable functions, create the cache here if it doesnt exist
+            $statically_memoizable(typeof(func)) && _get!($(esc(cache_constructor)), caches, func)
+            # verify we haven't switched the cache type
+            local cache_constructor_expr′ = _get!(()->$cache_constructor_expr, cache_constructor_exprs, func)
+            if cache_constructor_expr′ != $cache_constructor_expr
+                error("$func is already memoized with $cache_constructor_expr′")
+            end
+        end
+        # empty cache, but only if this is a top-level function definition
         # see also: https://discourse.julialang.org/t/is-there-a-way-to-determine-whether-code-is-toplevel
         $(esc(canary)) = true
         if isdefined($__module__, $(QuoteNode(canary)))

.github/workflows/CompatHelper.yml
@@ -1,0 +1,16 @@
+name: CompatHelper
+on:
+  schedule:
+    - cron: 0 0 * * *
+  workflow_dispatch:
+jobs:
+  CompatHelper:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Pkg.add("CompatHelper")
+        run: julia -e 'using Pkg; Pkg.add("CompatHelper")'
+      - name: CompatHelper.main()
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          COMPATHELPER_PRIV: ${{ secrets.COMPATHELPER_PRIV }}
+        run: julia -e 'using CompatHelper; CompatHelper.main()'

.github/workflows/TagBot.yml
@@ -1,9 +1,12 @@
 name: TagBot
 on:
-  schedule:
-    - cron: 0 * * * *
+  issue_comment:
+    types:
+      - created
+  workflow_dispatch:
 jobs:
   TagBot:
+    if: github.event_name == 'workflow_dispatch' || github.actor == 'JuliaTagBot'
     runs-on: ubuntu-latest
     steps:
       - uses: JuliaRegistries/TagBot@v1

.github/workflows/runtests.yml
@@ -1,0 +1,21 @@
+name: runtests
+
+on: [push, pull_request]
+
+jobs:
+  main:
+    runs-on: ${{ matrix.os }}
+    strategy:
+      matrix:
+        julia-version: ['1.0', '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '1.7-nightly']
+        os: [ubuntu-latest]
+      fail-fast: false
+
+    steps:
+      - uses: actions/checkout@v1.0.0
+      - uses: julia-actions/setup-julia@latest
+        with:
+          version: ${{ matrix.julia-version }}
+      - uses: julia-actions/julia-runtest@master
+        timeout-minutes: 20
+        continue-on-error: ${{ matrix.julia-version == '1.7-nightly' }}

.travis.yml
@@ -1,20 +1,0 @@
-language: julia
-
-os:
-  - linux
-
-julia:
-  - 1.0
-  - 1.1
-  - 1.2
-  - 1.3
-  - 1.4
-  - 1.5
-  - nightly
-
-matrix:
-  allow_failures:
-  - julia: nightly
-
-notifications:
-  email: false

Project.toml
@@ -6,12 +6,12 @@ version = "0.1.5"
 [deps]
 MacroTools = "1914dd2f-81c6-5fcd-8719-6d5c9610ff09"
 
+[compat]
+MacroTools = "^0.4.3, ^0.5"
+julia = "1"
+
 [extras]
 Test = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 
 [targets]
 test = ["Test"]
-
-[compat]
-julia = "1"
-MacroTools = "^0.4.3, ^0.5"

test/runtests.jl
@@ -1,96 +1,101 @@
 using Memoization
 using Test
 
+
 ##
 
 @testset "Memoization" begin
-    
-    local n
-    
-    @memoize f(x::T, y=nothing, args... ; z::Union{Int,Nothing}, w=nothing, kwargs...) where {T<:Int} = (n+=1; ((x,y,args...), (;z=z,w=w,kwargs...)))
-    
-    
-    # args
-    @test (n=0; f(1,    z=1) == ((1,nothing),(z=1,w=nothing)) && n==1)
-    @test (n=0; f(1,    z=1) == ((1,nothing),(z=1,w=nothing)) && n==0)
-    
-    @test (n=0; f(1,1,  z=1) == ((1,1),      (z=1,w=nothing)) && n==1)
-    @test (n=0; f(1,1,  z=1) == ((1,1),      (z=1,w=nothing)) && n==0)
-    
-    @test (n=0; f(1,1,1,z=1) == ((1,1,1),    (z=1,w=nothing)) && n==1)
-    @test (n=0; f(1,1,1,z=1) == ((1,1,1),    (z=1,w=nothing)) && n==0)
-    
-    # kwargs
-    @test (n=0; f(1,z=1,w=1)     == ((1,nothing),(z=1,w=1))     && n==1)
-    @test (n=0; f(1,z=1,w=1)     == ((1,nothing),(z=1,w=1))     && n==0)
-    
-    @test (n=0; f(1,z=1,w=1,p=1) == ((1,nothing),(z=1,w=1,p=1)) && n==1)
-    @test (n=0; f(1,z=1,w=1,p=1) == ((1,nothing),(z=1,w=1,p=1)) && n==0)
-    
-    # cache clear
-    Memoization.empty_cache!(f)
-    @test (n=0; f(1,z=1) == ((1,nothing),(z=1,w=nothing)) && n==1)
-    Memoization.empty_cache!(f)
-    @test (n=0; f(1,z=1) == ((1,nothing),(z=1,w=nothing)) && n==1)
-    
-    # Dict vs. IdDict cache vs. custom cache
-    @memoize IdDict g(x) = (n+=1; x)
-    @test (n=0; g([1,2])==[1,2] && n==1)
-    @test (n=0; g([1,2])==[1,2] && n==1)
-    @memoize   Dict g(x) = (n+=1; x)
-    @test (n=0; g([1,2])==[1,2] && n==1)
-    @test (n=0; g([1,2])==[1,2] && n==0)
-    @memoize Dict() g(x) = (n+=1; x)
-    @test (n=0; g([1,2])==[1,2] && n==1)
-    @test (n=0; g([1,2])==[1,2] && n==0)
-    
-    # redefinition
-    # should only clear cache in toplevel, hence @eval
-    @eval @memoize h(x) = x
-    @test @eval h(2)==2 && h(2)==2
-    @eval @memoize h(x) = 2x
-    @test @eval h(2)==4 && h(2)==4
-    
-    # inference
-    @test @inferred((@memoize foo(x) = x)(2)) == 2
-    # this is broken because @inferred is failing despite @code_warntype giving that its inferred:
-    @test_broken @inferred((@memoize foo(;x) = x)(x=2)) == 2
-    
-    # closures
-    function make_func(x)
-        @memoize func(y) = (n+=1; (x,y))
-    end
-    k = make_func(1)
-    @test (n=0; k(2)==(1,2) && n==1)
-    @test (n=0; k(2)==(1,2) && n==0)
-    k′ = make_func(2)
-    @test (n=0; k′(2)==(2,2) && n==1)
-    @test (n=0; k′(2)==(2,2) && n==0)
-    @test (n=0; k(2)==(1,2) && n==0)
-    
-    # callables
-    @eval struct Baz{T} end
-    @memoize (::Baz{T})(x::X) where {X, T<:Int} = (n+=1; Int)
-    @memoize (::Baz{T})(x::X) where {X, T<:String} = (n+=1; String)
-    bazint = Baz{Int}()
-    bazstring = Baz{String}()
-    @test (n=0; bazint(2)==Int && n==1)
-    @test (n=0; bazint(2)==Int && n==0)
-    @test (n=0; bazstring(2)==String && n==1)
-    @test (n=0; bazstring(2)==String && n==0)
-    Memoization.empty_cache!(Baz{Int})
-    @test (n=0; bazint(2)==Int && n==1)
-    Memoization.empty_cache!(Baz{String})
-    @test (n=0; bazstring(2)==String && n==1)
-    Memoization.empty_cache!(Baz)
-    @test (n=0; bazint(2)==Int && n==1)
-    @test (n=0; bazstring(2)==String && n==1)
-    
-    # unnamed args
-    @memoize uarg(::Type{T}) where {T} = (n+=1; T)
-    @test (n=0; uarg(Int)==Int && n==1)
-    @test (n=0; uarg(Int)==Int && n==0)
-    @test (n=0; uarg(Float64)==Float64 && n==1)
-    @test (n=0; uarg(Float64)==Float64 && n==0)
-    
+
+global n = 0
+
+@eval @memoize f(x::T, y=nothing, args... ; z::Union{Int,Nothing}, w=nothing, kwargs...) where {T<:Int} = (global n+=1; ((x,y,args...), (;z=z,w=w,kwargs...)))
+
+# args
+n=0; @test (f(1,    z=1) == ((1,nothing),(z=1,w=nothing)) && n==1)
+n=0; @test (f(1,    z=1) == ((1,nothing),(z=1,w=nothing)) && n==0)
+
+n=0; @test (f(1,1,  z=1) == ((1,1),      (z=1,w=nothing)) && n==1)
+n=0; @test (f(1,1,  z=1) == ((1,1),      (z=1,w=nothing)) && n==0)
+
+n=0; @test (f(1,1,1,z=1) == ((1,1,1),    (z=1,w=nothing)) && n==1)
+n=0; @test (f(1,1,1,z=1) == ((1,1,1),    (z=1,w=nothing)) && n==0)
+
+# kwargs
+n=0; @test (f(1,z=1,w=1)     == ((1,nothing),(z=1,w=1))     && n==1)
+n=0; @test (f(1,z=1,w=1)     == ((1,nothing),(z=1,w=1))     && n==0)
+
+n=0; @test (f(1,z=1,w=1,p=1) == ((1,nothing),(z=1,w=1,p=1)) && n==1)
+n=0; @test (f(1,z=1,w=1,p=1) == ((1,nothing),(z=1,w=1,p=1)) && n==0)
+
+# cache clear
+Memoization.empty_cache!(f)
+n=0; @test (f(1,z=1) == ((1,nothing),(z=1,w=nothing)) && n==1)
+Memoization.empty_cache!(f)
+n=0; @test (f(1,z=1) == ((1,nothing),(z=1,w=nothing)) && n==1)
+
+# Dict vs. IdDict cache vs. custom cache
+@eval @memoize IdDict g1(x) = (global n+=1; x)
+n=0; @test (g1([1,2])==[1,2] && n==1)
+n=0; @test (g1([1,2])==[1,2] && n==1)
+@eval @memoize   Dict g2(x) = (global n+=1; x)
+n=0; @test (g2([1,2])==[1,2] && n==1)
+n=0; @test (g2([1,2])==[1,2] && n==0)
+@eval @memoize Dict() g3(x) = (global n+=1; x)
+n=0; @test (g3([1,2])==[1,2] && n==1)
+n=0; @test (g3([1,2])==[1,2] && n==0)
+
+# redefinition
+# should only clear cache in toplevel, hence @eval
+@eval @memoize h(x) = x
+@test h(2)==2 && h(2)==2
+@eval @memoize h(x) = 2x
+@test h(2)==4 && h(2)==4
+
+# inference
+@eval @memoize foo(x) = x
+@eval @memoize foo(;x) = x
+@test @inferred(foo(2)) == 2
+@test @inferred(foo(x=2)) == 2
+
+# closures
+function make_func(x)
+    @memoize func(y) = (global n+=1; (x,y))
+end
+k = make_func(1)
+n=0; @test (k(2)==(1,2) && n==1)
+n=0; @test (k(2)==(1,2) && n==0)
+k′ = make_func(2)
+n=0; @test (k′(2)==(2,2) && n==1)
+n=0; @test (k′(2)==(2,2) && n==0)
+n=0; @test (k(2)==(1,2) && n==0)
+
+# callables
+@eval struct Baz{T} end
+@eval @memoize (::Baz{T})(x::X) where {X, T<:Int} = (global n+=1; Int)
+@eval @memoize (::Baz{T})(x::X) where {X, T<:String} = (global n+=1; String)
+bazint = Baz{Int}()
+bazstring = Baz{String}()
+n=0; @test (bazint(2)==Int && n==1)
+n=0; @test (bazint(2)==Int && n==0)
+n=0; @test (bazstring(2)==String && n==1)
+n=0; @test (bazstring(2)==String && n==0)
+Memoization.empty_cache!(Baz{Int})
+n=0; @test (bazint(2)==Int && n==1)
+Memoization.empty_cache!(Baz{String})
+n=0; @test (bazstring(2)==String && n==1)
+Memoization.empty_cache!(Baz)
+n=0; @test (bazint(2)==Int && n==1)
+n=0; @test (bazstring(2)==String && n==1)
+
+# unnamed args
+@eval @memoize uarg(::Type{T}) where {T} = (global n+=1; T)
+n=0; @test (uarg(Int)==Int && n==1)
+n=0; @test (uarg(Int)==Int && n==0)
+n=0; @test (uarg(Float64)==Float64 && n==1)
+n=0; @test (uarg(Float64)==Float64 && n==0)
+
+# redefining cache type
+@eval @memoize IdDict redef_cache_toplevel(x) = x
+@test_throws Exception @eval @memoize Dict redef_cache_toplevel(x) = x
+
 end

commit e98f3b39108d3ca4928471180330ff219408b826

fix bug introduced in #12 for precompiled modules

src/Memoization.jl
@@ -6,22 +6,43 @@ export @memoize
 # Stores a mapping (func => cache) for each memoized function `func`.
 const caches = IdDict()
 
-# Stores a mapping (cache_constructor_exprs => cache) for each
-# memoized function `func`, so that we can verify the same expression 
+# Stores a mapping (func => cache_constructor_expr) for each
+# memoized function `func`, so that we can verify the same expression
+# is always used.
 const cache_constructor_exprs = IdDict()
 
-# Non-function "callables" or any function that has closed-over variables
-# attached to it (indicated by having type parameters) are memoized
-# per-instance. 
+
+# Only top-level functions are memoized "statically", meaning that
+# get_cache(_,foo) for a top-level function, foo, does *not* result in
+# looking up Memoization.caches[foo] at run-time; the lookup is moved
+# to compile-time with the generated function trickery below. For
+# memoized closures (which can be recognized as functions with type
+# parameters) or callables (both where we have to look up a specific
+# *instance*), the lookup has to be "dynamic" i.e. done at run-time.
 statically_memoizable(::Type{F}) where {F<:Function} = isempty(F.parameters)
 statically_memoizable(::Type) = false
 
 
-# Look up the cache for a given memoized function. Using a generated
-# function allows us to move this lookup to compile time for
-# statically memoizable functions and improve performance.
+# Look up the cache for a given memoized function. The
+# generated-function trickery here allows us to move this lookup to
+# compile-time for statically memoizable functions and improve
+# performance.
 @generated function get_cache(default::Base.Callable, func::F) where {F}
-    statically_memoizable(F) ? caches[F.instance] : :(_get!(default, $caches, func))
+    if statically_memoizable(F)
+        # If the function is statically memoizable, the first
+        # lookup is dynamic, but we also define a specialized
+        # get_cache method which makes every subsequent lookup
+        # static.
+        quote
+            @eval @generated get_cache(::Base.Callable, ::$F) = caches[$(F.instance)]
+            _get!(default, $caches, func)
+        end
+    else
+        # For callables and closures, every lookup has to be dynamic. 
+        quote
+            _get!(default, $caches, func)
+        end
+    end
 end
 
 """
@@ -63,11 +84,11 @@ macro memoize(ex1, ex2=nothing)
     cache_constructor, funcdef = ex2 == nothing ? (IdDict, ex1) : (ex1, ex2)
     sdef = splitdef(funcdef)
     cache_constructor_expr = QuoteNode(Base.remove_linenums!(cache_constructor))
-    # if cache_constructor is a call, wrap it in a () -> ...
+    # if cache_constructor is a call, wrap it in a () -> ...to make it a callable
     if isexpr(cache_constructor, :call)
         cache_constructor = :(() -> $cache_constructor)
     end
-    # give unnamed args a placeholder name:
+    # give unnamed args placeholder names
     sdef[:args] = map(sdef[:args]) do arg
         sarg = splitarg(arg)
         combinearg((sarg[1] == nothing ? gensym() : sarg[1]), sarg[2:end]...)
@@ -100,9 +121,7 @@ macro memoize(ex1, ex2=nothing)
     quote
         func = Core.@__doc__ $(esc(combinedef(sdef)))
         begin
-            # for statically memoizable functions, create the cache here if it doesnt exist
-            $statically_memoizable(typeof(func)) && _get!($(esc(cache_constructor)), caches, func)
-            # verify we haven't switched the cache type
+            # verify we haven't already memoized this function with a different cache type
             local cache_constructor_expr′ = _get!(()->$cache_constructor_expr, cache_constructor_exprs, func)
             if cache_constructor_expr′ != $cache_constructor_expr
                 error("$func is already memoized with $cache_constructor_expr′")